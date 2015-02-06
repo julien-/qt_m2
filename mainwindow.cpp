5164ada4255cab81add6025f7382739483f3f526
julien- <julien.tekrane@laposte.net>
2015-02-06 12:32:45 +0100

    First commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,14 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    scene = new QGraphicsScene(this);
+    ui->graphicsView->setScene(scene);
+
+
+    // create our object and add it to the scene
+    item = new MyItem(  ui->graphicsView->width(),  ui->graphicsView->height());
+    scene->addItem(item);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsScene>
+#include "myitem.h"
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +19,9 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    QGraphicsScene *scene;
+    MyItem *item;
 };
 
 #endif // MAINWINDOW_H
